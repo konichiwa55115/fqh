99cd34dc42b2116794048edffcd3ea615ff84548
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2024-03-01 00:09:09 +0200

    Update config.py

--- teletweet/config.py
+++ teletweet/config.py
@@ -9,11 +9,11 @@ __author__ = "Benny <benny.think@gmail.com>"
 
 import os
 
-BOT_TOKEN = os.getenv("TOKEN", "fghjk789")
-APP_ID = int(os.getenv("APP_ID", "456fgh78"))
-APP_HASH = os.getenv("APP_HASH", "456gfhj78")
+BOT_TOKEN = os.getenv("TOKEN", "5782497998:AAFdx2dX3yeiyDIcoJwPa_ghY2h_dozEh_E")
+APP_ID = int(os.getenv("APP_ID", "17983098"))
+APP_HASH = os.getenv("APP_HASH", "ee28199396e0925f1f44d945ac174f64")
 
-CONSUMER_KEY = os.getenv("CONSUMER_KEY", "456fghj78")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET", "45678fghj")
+CONSUMER_KEY = os.getenv("CONSUMER_KEY", "i72lBPnd7xZlKYOl1ELaeUgt4")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET", "TE6STQVwjok9caTV1g8jjyPmxlbNnN4TbIdJ8GNmkiTFfqEoC4")
 
 tweet_format = "https://twitter.com/{screen_name}/status/{id}"
